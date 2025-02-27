8694494a64d91bd3c49293810ac9d3f85ad686ae
HienTD <hien.td@malitools.asia>
2025-02-27 15:10:24 +0700

    Create DSU.cpp

---
+++ CODE/DSU.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Link: https://marisaoj.com/problem/130
+ * Code được tham khảo từ: ...
+ */
+
+int N, Q;
+
+struct DisjointSetUnion{
+  vector<int> sz;
+  vector<int> par;
+  int N;
+
+  DisjointSetUnion(int sizes){
+    N = sizes;
+    sz.assign(N + 1, 1);
+    par.assign(N + 1, -1);
+  }
+
+  int root(int u){
+    return (par[u] < 0 ? u : par[u] = root(par[u]));
+  }
+
+  bool merge(int u, int v){
+    u = root(u);
+    v = root(v);
+    if(u == v) return false;
+    if(sz[v] > sz[u]) swap(u, v);
+    sz[u] += sz[v];
+    par[v] = u;
+    return true;
+  }
+};
+
+int main(void){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N >> Q;
+  DisjointSetUnion dsu(N);
+  while(Q --){
+    int t;
+    cin >> t;
+    if(t == 1){
+      int u, v;
+      cin >> u >> v;
+      dsu.merge(u, v);
+    }
+    else{
+      int u, v;
+      cin >> u >> v;
+      cout << (dsu.root(u) == dsu.root(v) ? "YES" : "NO") << '\n';
+    }
+  }
+  
+  return 0;
+}
