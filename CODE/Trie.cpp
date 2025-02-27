9932e598c3335a9aca71977336f79d2c605aedcf
HienTD <hien.td@malitools.asia>
2025-02-27 10:34:19 +0700

    Create Trie.cpp

---
+++ CODE/Trie.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Link: https://marisaoj.com/problem/237
+ * Bài này vì lí do Trie ăn quá nhiều bộ nhớ trong MarisaOJ chỉ chấp nhận 256MB nên chúng ta phải dùng cấu trúc mảng động để tạo cây
+ * Ngược lại nếu như trong mọi trường hợp có thể (số lượng node của cây tối đa 10^6) thì chúng ta nên sử dụng mảng cứng để tiết kiệm thời gian
+ * Vì hàm new trong C++ chạy rất chậm (nếu là cây có rất nhiều node thì thời gian chạy có thể gấp 3-4 lần dùng mảng cứng)
+ * Vì Trie là cấu trúc dữ liệu chạy bằng con trỏ nên nó ăn rất nhiều bộ nhớ + thời gian chạy (mỗi con trỏ là 8 bytes)
+ */
+
+const int ALPHABET = 26;
+const int mxN = 1e6 + 5;
+
+int N;
+bool ok = false;
+
+struct Trie{
+  struct Node{
+    Node* child[ALPHABET];
+    Node(){
+      for(int i = 0; i <= 25; ++ i){
+        child[i] = nullptr;
+      }
+    }
+  };
+
+  // int TrieNodeCnt = 0;
+  Node* root = new Node();
+  // root = createNode();
+  // Node nodes[mxN];
+
+  // Node* createNode(void){
+  //   return &nodes[TrieNodeCnt ++];
+  // }
+
+  void add(const string &s){
+    Node* p = root;
+    bool temp = true;
+    for(char c : s){
+      if(p->child[c - 'a'] == nullptr){
+        p->child[c - 'a'] = new Node();
+        temp = false;
+      }
+      p=p->child[c - 'a'];
+    }
+    ok |= temp;
+  }
+};
+
+int main(void){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N;
+  Trie trie;
+  for(int i = 1; i <= N; ++ i){
+    string s;
+    cin >> s;
+    trie.add(s);
+    if(ok == true){
+      cout << "YES";
+      return 0;
+    }
+  }
+  cout << "NO";
+  
+  return 0;
+}
